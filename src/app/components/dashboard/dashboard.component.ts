3d1991e574ad3a8e8b757710206f97c01f0627cb
roberto14118927 <robertoruiz141189@gmail.com>
2018-11-22 13:40:12 -0600

    Actiulizaciones!

--- src/app/components/dashboard/dashboard.component.ts
+++ src/app/components/dashboard/dashboard.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { DataApiService } from 'src/app/services/data-api.service';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-dashboard',
@@ -7,9 +9,18 @@ import { Component, OnInit } from '@angular/core';
 })
 export class DashboardComponent implements OnInit {
 
-  constructor() { }
+  constructor(private dataApiService: DataApiService, private router: Router) { }
 
   ngOnInit() {
+    return this.dataApiService.getAllTest()
+    .subscribe(data => {
+      console.log(data);
+      //this.router.navigate(['dashboard']);
+    },
+      error => {
+        console.log(error.error.detail);
+      }
+    )
   }
 
 }
--- src/app/services/data-api.service.ts
+++ src/app/services/data-api.service.ts
@@ -1,9 +1,27 @@
 import { Injectable } from '@angular/core';
+import { HttpClient, HttpHeaders } from '@angular/common/http';
+import { Observable } from 'rxjs/internal/Observable';
+import { map } from 'rxjs/operators';
+
+import {AuthService} from './auth.service';
 
 @Injectable({
   providedIn: 'root'
 })
 export class DataApiService {
 
-  constructor() { }
+  constructor(private http: HttpClient, private authService: AuthService) { }
+  Tests: Observable<any>;
+  Test: Observable<any>;
+
+  headers: HttpHeaders = new HttpHeaders({
+    'Content-Type': 'application/json',
+    'Authorization': 'Token 2a2b44a1efc726b0151cb0010f5be84fbc709f28'
+  })
+
+  getAllTest (){
+    const url_api = 'http://localhost:8000/api/v1/test1/'
+    return this.http.get(url_api, {headers: this.headers})
+    .pipe(map(data => data))
+  }
 }
